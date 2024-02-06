94826f62eef2e8ca765ef0161736817bbc7a2d86
ajushrajiv <152269571+ajushrajiv@users.noreply.github.com>
2024-02-06 14:46:42 +0100

    created git project and added navbar

---
+++ todo/src/navbar/Navbar.js
@@ -0,0 +1,11 @@
+import styles from "./Navbar.module.css";
+
+function Navbar(){
+    return(
+        <div className={styles.maincontainer}>
+            <h5>TODO List</h5>
+        </div>
+    )
+}
+
+export default Navbar;
