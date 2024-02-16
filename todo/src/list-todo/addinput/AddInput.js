f196e8021ee932bb2a895393fa82487febb86611
ajushrajiv <152269571+ajushrajiv@users.noreply.github.com>
2024-02-16 16:01:40 +0100

    display fetched data

--- todo/src/list-todo/addinput/AddInput.js
+++ todo/src/list-todo/addinput/AddInput.js
@@ -1,9 +1,24 @@
-import StandardForm from "../../components/forms/standardform/StandardForm";
+import TodoContainer from "../../components/container/todocontainer/TodoContainer";
+import { useEffect, useState } from "react";
+import styles from "./AddInput.module.css"
 
 function AddInput() {
+
+  const [ todos, setTodos ] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5030/v1/todo/todos")
+    .then((response) => response.json())
+    .then((jsonres) => {
+      console.log(jsonres.todos[0]);
+      setTodos(jsonres.todos[0]);
+    })
+    }, [])
+  
+
   return (
-    <div>
-      <StandardForm />
+    <div className = {styles.main}>
+      <TodoContainer todo={todos}></TodoContainer> 
     </div>
   );
 }
